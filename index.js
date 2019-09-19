1cf2867723b4c92ccb3b5862eea2236492fb8780
mevinyanuar <mmevinalifviyanuar@gmail.com>
2019-09-19 17:11:00 +0700

    push dua

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const app = express()
 const indexRouter = require("./routes/index")
 const newUser = require("./routes/user")
 const bodyParser = require('body-parser')
+const PORT = process.env.PORT || 3421
 
 app.use(bodyParser.urlencoded({extended: false}))
 
 app.use("/", indexRouter)
 app.use("/user", newUser)
 
-app.listen(3421, ()=> console.log('server running on port 3421'))
+app.listen(PORT, ()=> console.log('server running on port 3421'))
